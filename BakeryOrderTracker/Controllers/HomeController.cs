9f77140901eddbb6d0a5a54ce2a6542528505f35
Megan McKissack <meganmckissack@gmail.com>
2022-07-16 18:58:39 -0700

    crate vendor contollers and views, update home controller

--- BakeryOrderTracker/Controllers/HomeController.cs
+++ BakeryOrderTracker/Controllers/HomeController.cs
@@ -9,5 +9,6 @@ namespace BakeryOrderTracker.Controllers
     {
       return View();
     }
+
   }
 }
---
+++ BakeryOrderTracker/Controllers/VendorsController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using BakeryOrderTracker.Models;
+
+namespace BakeryOrderTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+  }
+}
