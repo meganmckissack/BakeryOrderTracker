a2c1e530664e4b95a09eededb5437e77f5490694
Megan McKissack <meganmckissack@gmail.com>
2022-07-16 19:21:36 -0700

    create vendor post route

--- BakeryOrderTracker/Controllers/VendorsController.cs
+++ BakeryOrderTracker/Controllers/VendorsController.cs
@@ -19,5 +19,12 @@ namespace BakeryOrderTracker.Controllers
     {
       return View();
     }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
   }
 }
